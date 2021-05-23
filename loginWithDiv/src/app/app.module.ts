cc3dd93b82a496bd8985c13f8cb5d5ca6c854979
Moinak Ghosh <moinak.ghosh4321@gmail.com>
2021-05-23 21:52:43 +0530

    initial

---
+++ loginWithDiv/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginRightComponent } from './login-right/login-right.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginRightComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
